cedae0661e9845235e2bcda43de0d4a3aa87efab
Andrei Mocica <andreimocica@192-168-0-56.rdsnet.ro>
2023-02-24 08:51:54 +0200

    Sa avem si noi cron
    (cherry picked from commit 59565028554d0e2d26509c829a125f5696d99e8f)

---
+++ Controller/Feed/Cron.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Retargeting\Tracker\Controller\Feed;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Retargeting\Tracker\Helper\FeedHelper;
+
+class Cron extends Action {
+
+    public function __construct(Context $context, FeedHelper $_retargetingFeed) {
+        parent::__construct($context);
+        $this->_retargetingFeed = $_retargetingFeed;
+    }
+
+    public function execute()
+    {
+        $this->_retargetingFeed->cronFeed();
+
+        header("Content-type: text/json; charset=utf-8");
+
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+        header('Pragma: public');
+
+        echo '{"status":"success"}';
+    }
+}
+
