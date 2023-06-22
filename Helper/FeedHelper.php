911c65c8d72d8780cf1fba911f345d1731825695
unknown <andrei.mocica@retargeting.biz>
2023-06-22 01:51:21 -0700

    Added cast float

--- Helper/FeedHelper.php
+++ Helper/FeedHelper.php
@@ -383,7 +383,7 @@ class FeedHelper extends AbstractHelper
 
     private function getProductWeight($product) {
         return number_format($this->formatWeightToKg($this->getWeightUnit(),$product->getWeight()), 2, '.', '') > 0
-            ? number_format($this->formatWeightToKg($this->getWeightUnit(),$product->getWeight()), 2, '.', '') : 0.01;
+            ? (float)number_format($this->formatWeightToKg($this->getWeightUnit(),$product->getWeight()), 2, '.', '') : 0.01;
     }
 
 
